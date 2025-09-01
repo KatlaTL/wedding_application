50c7ace2e8a30da89f0cc7c71b5aabf0f20f08cb
Asger <regsa13@gmail.com>
2025-09-01 14:36:09 +0200

    updated the button styling a bit and added react functionality to the invitation component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import type { LucideProps } from "lucide-react";
 import type { ButtonHTMLAttributes } from "react";
 
 interface ButtoneProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: "primary" | "secondary";
+    variant?: "primary" | "secondary" | "secondary-no-hover" | "destructive";
     size?: "small" | "medium" | "large";
     icon?: React.ForwardRefExoticComponent<Omit<LucideProps, "ref">>;
 }
@@ -16,11 +16,24 @@ const Button: React.FC<ButtoneProps> = ({
     ...props
 }) => {
 
-    const baseStyle = "w-full rounded-lg bg-background border border-primary hover:cursor-pointer";
+    const baseStyle = "w-full rounded-lg bg-background border border-primary hover:cursor-pointer outline-none";
 
-    const variantStyle = variant === "primary"
-        ? "text-color-text hover:bg-tertiary/90 hover:border-[var(--color-tertiary)]"
-        : "bg-primary hover:text-primary/90 hover:bg-background text-background hover:shadow-lg active:shadow-none";
+    let variantStyle = "";
+
+    switch (variant) {
+        case "primary":
+            variantStyle = "text-color-text hover:bg-tertiary/90 hover:border-[var(--color-tertiary)]";
+            break;
+        case "secondary":
+            variantStyle = "bg-primary hover:text-primary/90 hover:bg-background text-background hover:shadow-lg active:shadow-none";
+            break;
+        case "secondary-no-hover":
+            variantStyle = "bg-primary text-background";
+            break;
+        case "destructive":
+            variantStyle = "bg-destructive  text-white"
+            break;
+    }
 
     let sizeStyle = "";
 
@@ -39,8 +52,8 @@ const Button: React.FC<ButtoneProps> = ({
     const buttonIconStyle = Icon ? "flex items-center justify-center gap-3" : "";
 
     const iconStyle = variant === "primary"
-        ? "text-color-text h-[11px] w-[11px] mb-[0.5px]"
-        : "text-primary h-[11px] w-[11px] mb-[0.5px]";
+        ? "h-[11px] w-[11px] mb-[0.5px]"
+        : "h-[11px] w-[11px] mb-[0.5px]";
 
     return (
         <button
--- src/pages/Invitation/Invitation.tsx
+++ src/pages/Invitation/Invitation.tsx
@@ -8,22 +8,38 @@ import HeadingWithIcon from "../../components/HeadingWithIcon";
 import ButtonGroup from "./_components/ButtonGroup";
 import Wrapper from "./_components/Wrapper";
 import { Checkbox, CheckboxIndicator } from "../../components/Checkbox";
+import { useState } from "react";
+import type { CheckedState } from "@radix-ui/react-checkbox";
 
 const Invitation = () => {
+    const [attendee, setAttendee] = useState<string>();
+    const [isAttending, setIsAttending] = useState<boolean>();
+    const [needLift, setNeedLift] = useState<boolean>();
+    const [canOfferLift, setCanOfferLift] = useState<boolean>();
+    const [dietary, setDietary] = useState<string>();
+    const [allergies, setAllergies] = useState<string>();
+
+    const invitations: string[] = ["Asger", "Rikke", "Jakob", "Morten", "Frederik"];
 
     const dietaryOptions: string[] = ["Veganer", "Vegetar", "Altspisende"];
 
+    const handleCheckedChange = (checked: CheckedState, value: string) => {
+        if (checked) {
+            setDietary(value);
+        }
+    }
+
     return (
         <Section title="Bekræft deltagelse" description="Lad os vide, om du kommer!">
+            <form onSubmit={(e) => e.preventDefault()}>
+                <div className="bg-background-muted rounded-lg border-primary-30 border p-5 w-120">
+                    <div className="flex flex-col items-start text-left gap-3">
+                        <HeadingWithIcon icon={Users} text="Tilmelding" />
 
-            <div className="bg-background-muted rounded-lg border-primary-30 border p-5 w-120">
-                <div className="flex flex-col items-start text-left gap-3">
-                    <HeadingWithIcon icon={Users} text="Tilmelding" />
-
-                    <Wrapper className="mb-2">
+                        <Wrapper className="mb-2">
                             <p className="!text-color-text">Find dit navn</p>
 
-                            <Select>
+                            <Select onValueChange={(v) => setAttendee(v)}>
                                 <SelectTrigger className="SelectTrigger rounded-lg cursor-pointer flex w-full border-primary border outline-none text-left text-xs px-1.5 py-1 text-muted-foreground justify-between focus:outline-none" aria-label="Name">
                                     <SelectValue placeholder="Vælg dit navn på listen" />
                                     <SelectIcon className="SelectIcon">
@@ -38,9 +54,7 @@ const Invitation = () => {
                                         </SelectScrollUpButton>
 
                                         <SelectViewport>
-                                            <SelectItem value="asger">Asger</SelectItem>
-                                            <SelectItem value="rikke">Rikke</SelectItem>
-                                            <SelectItem value="jakob">Jakob</SelectItem>
+                                            {invitations.map(value => <SelectItem value={value}>{value}</SelectItem>)}
                                         </SelectViewport>
 
                                         <SelectScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-muted-foreground">
@@ -48,69 +62,75 @@ const Invitation = () => {
                                         </SelectScrollDownButton>
                                     </SelectContent>
                                 </SelectPortal>
-
                             </Select>
-                    </Wrapper>
+                        </Wrapper>
+                        {attendee && (
+                            <Wrapper>
+                                <ButtonGroup title="Deltager du?">
+                                    <Button size="small" icon={CheckCircle} variant={isAttending ? "secondary-no-hover" : "primary"} onClick={() => setIsAttending(true)}>Ja, jeg kommer</Button>
+                                    <Button size="small" icon={XCircle} variant={isAttending === false ? "destructive" : "primary"} onClick={() => setIsAttending(false)}>Nej, desværre ikke</Button>
+                                </ButtonGroup>
+                            </Wrapper>
+                        )}
 
-                    <Wrapper>
-                        <ButtonGroup title="Deltager du?">
-                            <Button size="small" icon={CheckCircle}>Ja, jeg kommer</Button>
-                            <Button size="small" icon={XCircle}>Nej, desværre ikke</Button>
-                        </ButtonGroup>
-                    </Wrapper>
+                        {isAttending && (
+                            <>
+                                <Wrapper className="bg-muted rounded-lg p-2">
+                                    <HeadingWithIcon icon={Car} text="Transport" className="mb-2" />
 
-                    <Wrapper className="bg-muted rounded-lg p-2">
-                        <HeadingWithIcon icon={Car} text="Transport" className="mb-2" />
+                                    <ButtonGroup title="Har du brug for et lift fra Odense?" className="mb-2">
+                                        <Button size="small" className="!w-auto px-3" variant={needLift ? "secondary-no-hover" : "primary"} onClick={() => setNeedLift(true)}>Ja, jeg har behov for et lift</Button>
+                                        <Button size="small" className="!w-auto px-3" variant={needLift === false ? "destructive" : "primary"} onClick={() => setNeedLift(false)}>Nej, jeg klare den</Button>
+                                    </ButtonGroup>
 
-                        <ButtonGroup title="Har du brug for et lift fra Odense?" className="mb-2">
-                            <Button size="small" className="!w-auto px-3">Ja, jeg har behov for et lift</Button>
-                            <Button size="small" className="!w-auto px-3">Nej, jeg klare den</Button>
-                        </ButtonGroup>
+                                    <ButtonGroup title="Kan du tilbyde et lift til andre gæster?">
+                                        <Button size="small" className="!w-auto px-3" variant={canOfferLift ? "secondary-no-hover" : "primary"} onClick={() => setCanOfferLift(true)}>Ja, jeg kan tilbyde et lift</Button>
+                                        <Button size="small" className="!w-auto px-3" variant={canOfferLift === false ? "destructive" : "primary"} onClick={() => setCanOfferLift(false)}>Nej, desværre ikke</Button>
+                                    </ButtonGroup>
+                                </Wrapper>
 
-                        <ButtonGroup title="Kan du tilbyde et lift til andre gæster?">
-                            <Button size="small" className="!w-auto px-3">Ja, jeg kan tilbyde et lift</Button>
-                            <Button size="small" className="!w-auto px-3">Nej, desværre ikke</Button>
-                        </ButtonGroup>
-                    </Wrapper>
 
-                    <Wrapper className="bg-muted rounded-lg p-2 !flex-row text-xs text-color-text">
-                        <form>
-                            <div className="flex gap-5">
-
-                                {dietaryOptions.map(value =>
-                                    <div className="flex gap-1 text-xs">
-                                        <Checkbox
-                                            className="flex size-4 items-center justify-center bg-background rounded border-primary outline-none"
-                                            id={value}
-                                        >
-                                            <CheckboxIndicator>
-                                                <CheckIcon className="size-4 bg-primary text-background-muted rounded" />
-                                            </CheckboxIndicator>
-                                        </Checkbox>
-                                        <label>
-                                            {value}
-                                        </label>
+                                <Wrapper className="bg-muted rounded-lg p-2 !flex-row text-xs text-color-text">
+                                    <div className="flex gap-5">
+                                        {dietaryOptions.map(value =>
+                                            <div className="flex gap-1 text-xs">
+                                                <Checkbox
+                                                    className="flex size-4 items-center justify-center bg-background rounded border-primary outline-none"
+                                                    onCheckedChange={(checked) => handleCheckedChange(checked, value)}
+                                                    checked={dietary === value}
+                                                    id={value}
+                                                >
+                                                    <CheckboxIndicator>
+                                                        <CheckIcon className="size-4 bg-primary text-background-muted rounded" />
+                                                    </CheckboxIndicator>
+                                                </Checkbox>
+                                                <label htmlFor={value}>
+                                                    {value}
+                                                </label>
+                                            </div>
+                                        )}
                                     </div>
-                                )}
-                            </div>
-                        </form>
-                    </Wrapper>
+                                </Wrapper>
 
-                    <Wrapper>
-                        <p className="!text-color-text">Har du kostrestriktioner eller allergier? (valgfrit)</p>
+                                <Wrapper>
+                                    <p className="!text-color-text">Har du kostrestriktioner eller allergier?</p>
 
-                        <textarea
-                            name="dietary"
-                            placeholder="Fortæl os gerne, hvis du har allergier eller særlige kostbehov"
-                            rows={3}
-                            className="rounded-lg px-2 pt-1 resize-none text-xs text-color-text placeholder-muted-foreground placeholder:text-xs border border-primary-30 focus:outline-primary"
-                        />
-                    </Wrapper>
+                                    <textarea
+                                        name="dietary"
+                                        value={allergies}
+                                        onChange={e => setAllergies(e.target.value)}
+                                        placeholder="Fortæl os gerne, hvis du har allergier eller særlige kostbehov"
+                                        rows={3}
+                                        className="rounded-lg px-2 pt-1 resize-none text-xs text-color-text placeholder-muted-foreground placeholder:text-xs border border-primary-30 focus:outline-primary"
+                                    />
+                                </Wrapper>
+                            </>
+                        )}
 
-                    <Button variant="secondary">Bekræft deltagelse</Button>
-
+                        <Button variant="secondary" className="mt-4">Bekræft deltagelse</Button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </Section>
     )
 }
